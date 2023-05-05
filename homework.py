806e1fb42c8b2480e2d33bacd0a45731461f48eb
Никита Соловьев <nvsolovev@gmail.com>
2023-05-05 22:28:25 +0300

    fixed code

--- homework.py
+++ homework.py
@@ -48,7 +48,7 @@ class Training:
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         raise NotImplementedError(
-                'Определите калории в %s.' % (self.__class__.__name__))
+            'Определите калории в %s.' % (self.__class__.__name__))
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
